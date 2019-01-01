b16b6939f8ee633238609092b715a16c29fd2410
SLJ <18234175208@163.com>
2019-01-01 17:21:53 +0800

    连续子数组的最大和

---
+++ src/go_offer/连续子数组的最大和/Solution.java
@@ -0,0 +1,32 @@
+package go_offer.连续子数组的最大和;
+
+/**
+ * @Description: HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。
+ * 今天测试组开完会后,他又发话了:
+ * 在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+ * 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+ * 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+ * 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+ *
+ * @create: 2019/1/1
+ * @Author: SLJ
+ */
+public class Solution {
+    /**
+     * 动态规划：
+     * 状态转移方程 : 以i结尾的数组arr, f(i) = max(f(i - 1) + arr[i] , arr[i])
+     * 最终结果res, res = max(f(i) , res)
+     * @param array
+     * @return
+     */
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int result = array[0];
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max + array[i], array[i]);
+            result = Math.max(max, result);
+        }
+        return result;
+    }
+}
